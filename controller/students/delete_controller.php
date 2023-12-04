cadc2b1d45dba611a1e9b6b8046fe57c57b2eff1
nabilchabab <nabil.chababnabil@gmail.com>
2023-12-04 14:58:55 +0100

    Updating The MVC Archithect

--- controller/students/delete_controller.php
+++ controller/students/delete_controller.php
@@ -1,12 +1,17 @@
 <?php
 require __DIR__ ."/../../connect.php";
+include  __DIR__ ."/../../model/functions.php";
 
 $id = $_GET["id"];
-$requet = "DELETE FROM `students` WHERE id = $id";
-$result = mysqli_query($connect, $requet);
+$requet = DeleteStudents();
+$result = $connect->prepare($requet);
 
 if ($result) {
-  header("Location: ../../view/admin/students/students.php?msg=Student deleted successfully");
+  $result->bind_param("i",$id);
+  if($result->execute()){
+    
+    header("Location: ../../view/admin/students/students.php?msg=Student deleted successfully");
+  }
 } else {
   echo "Failed: " . mysqli_error($connect);
 }
--- model/functions.php
+++ model/functions.php
@@ -4,7 +4,7 @@ function UpdateStudents(){
   return "UPDATE `students` SET `nom`=?,`prenom`=?,`cin`=?,`email`=?,`gender`=?  WHERE `id` = ?";
 }
 function DeleteStudents(){
-    return "";
+    return "DELETE FROM `students` WHERE id = ?";
 }
 
 function ShowStudents(){
@@ -15,8 +15,21 @@ function AddStudents(){
     return "INSERT INTO `students`(`nom`, `prenom`, `cin`, `email`, `gender` , `image`) VALUES (?,?,?,?,?,?)";
 }
 
+//Teachers
 function ShowTeachers(){
     return "SELECT * FROM `students` ORDER BY `nom` ASC";
+}
+
+function AddTeachers(){
+    return "INSERT INTO `students`(`nom`, `prenom`, `cin`, `email`, `gender` , `image`) VALUES (?,?,?,?,?,?)";
+}
+
+function UpdateTeachers(){
+    return "INSERT INTO `students`(`nom`, `prenom`, `cin`, `email`, `gender` , `image`) VALUES (?,?,?,?,?,?)";
+}
+
+function DeleteTeachers(){
+    return "INSERT INTO `students`(`nom`, `prenom`, `cin`, `email`, `gender` , `image`) VALUES (?,?,?,?,?,?)";
 }
   
 
